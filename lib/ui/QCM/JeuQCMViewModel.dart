559cac7b5b7acfa40bbf511efa09975a9ffa9d64
elhatriayoub <elhatriayoub059@gmail.com>
2026-01-12 17:05:03 +0100

    Mise à jour ViewModel QCM : récupération des données, total des questions et logique de chargement

--- lib/ui/QCM/JeuQCMViewModel.dart
+++ lib/ui/QCM/JeuQCMViewModel.dart
@@ -1,37 +1,27 @@
-import 'dart:async';
-
 import 'package:flutter/foundation.dart';
 import 'package:seriouse_game/models/QCM/qcm.dart';
 import 'package:seriouse_game/models/QCM/question.dart';
 import 'package:seriouse_game/models/QCM/reponse.dart';
 import 'package:seriouse_game/models/cours.dart';
 import 'package:seriouse_game/repositories/QCM/QCMRepository.dart';
- 
+
 class JeuQCMViewModel {
   Future<Map<String, dynamic>> recupererQCM(Cours cours, int selectedPageIndex) async {
     try {
       final qcmRepo = QCMRepository();
       List<int> idQCMList = await qcmRepo.getAllIdByCoursId(cours.id!);
       QCM? qcm = await qcmRepo.getById(idQCMList[selectedPageIndex]);
-      
-      Question? question;
-      List<Reponse>? reponses = [];
-      int? solution;
-      
+
       if (qcm == null || qcm.question == null || qcm.reponses == null) {
         throw Exception("QCM incomplet ou invalide");
       }
 
-      question = qcm.question;
-      reponses = qcm.reponses;
-      solution = qcm.numSolution;
-      
       return {
-        "question": question,
-        "options": reponses,
-        "correctAnswer": solution,
+        "question": qcm.question,
+        "options": qcm.reponses,
+        "correctAnswer": qcm.numSolution,
+        "totalQuestions": idQCMList.length,
       };
-    
     } catch (e) {
       if (kDebugMode) {
         print("Erreur lors du chargement du QCM : $e");
@@ -40,5 +30,3 @@ class JeuQCMViewModel {
     }
   }
 }
-
-   
