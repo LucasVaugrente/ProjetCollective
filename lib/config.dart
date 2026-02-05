de9092f989d44599bd34076a9d90e697ca1595f3
Lucas <lucasvaugrente35@gmail.com>
2026-02-05 21:05:43 +0100

    add config for url api

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,24 @@
+import 'dart:io';
+
 class AppConfig {
-  static const String apiBaseUrl = "https://ton-api.com";
+  // Configuration pour Android Emulator
+  static const String apiBaseUrlAndroidEmulator = "http://10.0.2.2:8000";
+
+  // Configuration pour iOS Simulator
+  static const String apiBaseUrlIosSimulator = "http://localhost:8000";
+
+  // Configuration pour appareil physique (WiFi)
+  static const String apiBaseUrlPhysicalDevice = "http://192.168.1.X:8000";
+
+  static const int apiTimeout = 10;
+
+  static String get effectiveApiUrl {
+    if (Platform.isAndroid) {
+      return apiBaseUrlAndroidEmulator;
+    } else if (Platform.isIOS) {
+      return apiBaseUrlIosSimulator;
+    } else {
+      return apiBaseUrlAndroidEmulator;
+    }
+  }
 }
